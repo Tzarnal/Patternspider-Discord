ddaab3b8bca4a74725023e3f13f210bb482eb85c
Xesyto <s.oudenaarden@gmail.com>
2017-07-18 18:14:24 +0200

    Added a coin flip plugin.

---
+++ PatternSpider - Discord/Plugins/Plugin-Coinflip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace PatternSpider_Discord.Plugins
+{
+    class PluginCoinflip : IPatternSpiderPlugin
+{
+    public string Name => "Coin Flip";
+    public List<string> Commands => new List<string> { "coin" };
+
+    private readonly DiceRoller _genie;
+
+    public PluginCoinflip()
+    {
+        _genie = new DiceRoller();
+    }
+
+    public async Task Command(string command, string messsage, SocketMessage m)
+    {
+        var roll = _genie.RollDice(2);
+
+        if (roll == 1)
+        {
+            await m.Channel.SendMessageAsync("Heads.");
+        }
+        else
+        {
+            await m.Channel.SendMessageAsync("Tails.");
+        }
+    }
+
+    public Task Message(string messsage, SocketMessage m)
+    {
+        return Task.CompletedTask;
+    }
+}
+}
