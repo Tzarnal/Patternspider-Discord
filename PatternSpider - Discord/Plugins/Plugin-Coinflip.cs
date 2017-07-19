0f37db2a5b74d34c73b848540959ebf9dba5d286
Xesyto <s.oudenaarden@gmail.com>
2017-07-19 17:32:27 +0200

    Fixed indentation issues.

--- PatternSpider - Discord/Plugins/Plugin-Coinflip.cs
+++ PatternSpider - Discord/Plugins/Plugin-Coinflip.cs
@@ -5,34 +5,34 @@ using Discord.WebSocket;
 namespace PatternSpider_Discord.Plugins
 {
     class PluginCoinflip : IPatternSpiderPlugin
-{
-    public string Name => "Coin Flip";
-    public List<string> Commands => new List<string> { "coin" };
-
-    private readonly DiceRoller _genie;
-
-    public PluginCoinflip()
     {
-        _genie = new DiceRoller();
-    }
+        public string Name => "Coin Flip";
+        public List<string> Commands => new List<string> { "coin" };
 
-    public async Task Command(string command, string message, SocketMessage m)
-    {
-        var roll = _genie.RollDice(2);
+        private readonly DiceRoller _genie;
 
-        if (roll == 1)
+        public PluginCoinflip()
         {
-            await m.Channel.SendMessageAsync("Heads.");
+            _genie = new DiceRoller();
         }
-        else
-        {
-            await m.Channel.SendMessageAsync("Tails.");
-        }
-    }
 
-    public Task Message(string message, SocketMessage m)
-    {
-        return Task.CompletedTask;
+        public async Task Command(string command, string message, SocketMessage m)
+        {
+            var roll = _genie.RollDice(2);
+
+            if (roll == 1)
+            {
+                await m.Channel.SendMessageAsync("Heads.");
+            }
+            else
+            {
+                await m.Channel.SendMessageAsync("Tails.");
+            }
+        }
+
+        public Task Message(string message, SocketMessage m)
+        {
+            return Task.CompletedTask;
+        }
     }
-}
 }
